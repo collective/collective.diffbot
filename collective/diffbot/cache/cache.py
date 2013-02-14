b7ec350c75eb051ffb5a5e6ef142636365f5aac1
Alin Voinea <alin@eaudeweb.ro>
2013-02-14 14:19:38 +0200

    Fix cache key for non-folderish objects

--- collective/diffbot/cache/cache.py
+++ collective/diffbot/cache/cache.py
@@ -3,5 +3,8 @@
 def cacheJsonKey(method, self, *args, **kwargs):
     """ Generate unique cache id
     """
-    name = getattr(self, '__name__', '')
-    return ':'.join((self.context.absolute_url(1), name))
+    return (
+        self.context.absolute_url(1),
+        getattr(self, '__name__', ''),
+        self.request.form
+    )
